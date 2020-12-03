96e2f8e7f848c0566965dc87bf3015581e7497d0
KarrinaB <carinabogomolowa@yandex.ru>
2020-12-03 11:53:22 +0300

    fixed code.

--- js/main.js
+++ js/main.js
@@ -1,31 +1,32 @@
 'use strict';
 
-let isNumber = function (n) {
+const isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-let randomNumber = Math.floor(Math.random() * 100);
- 
+const randomNumber = Math.floor(Math.random() * 100);
 
-const main = function() {
+const main = function () {
+
   let number = prompt("Введи число от 0 до 100:");
-    if (number > randomNumber) {
-      alert("Загаданное число меньше!");
-      main();
-  } if (number < randomNumber) {
-      alert("Загаданное число больше!");
-      main();
-  } if (!isNumber(number)) {
-      alert("Введи число!");
-      main();
-  } if (number == randomNumber) {
-      alert("Поздравляю, Ты угадал!");
-      return;
-  } if (number == null) {
-      alert("Игра окончена"); 
-      return;
-};
-сonsole.log(number);
+
+  if (number == null) {
+    alert("Игра окончена"); //если нажимаем отмену или ESC то в переменную возвращается значение null и появляется окно "Игра окончена"
+  } else if (!isNumber(number)) {
+    alert("Введи число!"); //иначе, если игрок вводит не число, а что либо другое, то появляется окно-введи число и функция запускается заново.
+    main();
+  } else if (number > randomNumber) {
+    alert("Загаданное число меньше!"); //иначе, если игрок ввел число, которое больше загаданного, появляется окно-число меньше и функция запускается заново
+    main();
+  } else if (number < randomNumber) {
+    alert("Загаданное число больше!");// аналогично предыдущему условию, только если игрок ввел число меньше загаданного
+    main();
+  };
+  if (number == randomNumber) {
+    alert("Поздравляю, Ты угадал!");//если введенное число совпало с загаданным, т.е. равно ему, то появляется окно Поздравляю, ты угадал,и переменная возвращается и игра начинается заново.
+    return;
+  };
+
 };
 
 main();
