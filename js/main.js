5dadae4435779f33585e8d1a5d53380c5902416b
KarrinaB <carinabogomolowa@yandex.ru>
2020-12-03 11:23:56 +0300

    Fixed code.

--- js/main.js
+++ js/main.js
@@ -4,27 +4,28 @@ let isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-let number, randomNumber = Math.floor(Math.random() * 100);
+let randomNumber = Math.floor(Math.random() * 100);
  
-while (number = prompt("Введи число от 0 до 100:")) {
-   if (!isNumber(number)) {
-      alert("Введи число!");
 
-    } else if (number > randomNumber) {
+const main = function() {
+  let number = prompt("Введи число от 0 до 100:");
+    if (number > randomNumber) {
       alert("Загаданное число меньше!");
-
-    } else if (number < randomNumber) {
+      main();
+  } if (number < randomNumber) {
       alert("Загаданное число больше!");
-
-    } else if (number == randomNumber) {
+      main();
+  } if (!isNumber(number)) {
+      alert("Введи число!");
+      main();
+  } if (number == randomNumber) {
       alert("Поздравляю, Ты угадал!");
-      //break;
-    };
-  };
-      if (number == null) {
-        alert("Игра окончена"); 
-    };
-  
-        
+      return;
+  } if (number == null) {
+      alert("Игра окончена"); 
+      return;
+};
+сonsole.log(number);
+};
 
-
+main();
